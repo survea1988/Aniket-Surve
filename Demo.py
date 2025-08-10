0e441588abf6389e1a5d367fc158085ec17bc33d
survea <aniket.surve1@gmail.com>
2025-08-11 00:14:49 +0530

    first commit

---
+++ Demo.py
@@ -0,0 +1,14 @@
+# First program
+
+print("Aniket Surve")
+s = {10, 50, 20}
+print(s)
+print(type(s))
+
+# typecasting list to set
+s = set(["a", "b", "c"])
+print(s)
+
+# Adding element to the set
+s.add("d")
+print(s)
