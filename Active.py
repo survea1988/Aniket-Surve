c1536e90480e360332b00bdd8f48ea0897bc68fd
Aniket Surve <aniket.surve1@gmail.com>
2026-07-12 20:40:17 +0530

    Age Bracket

---
+++ Active.py
@@ -0,0 +1,14 @@
+# Taking input
+age = int(input())
+
+# Write your code here
+if (age<13):
+    result = "child(<13)"
+elif (age>=13 and age <=17):
+    result = "Teen (13-17)"
+elif (age>18):
+    result = "Adult(18+)"
+else:
+    result = "Invalid"    
+# Print the output
+print(result)
